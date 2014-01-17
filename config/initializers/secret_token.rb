3a55050846dee6604861f4d0bbd149263feaa80a
John Johnson <dampboxdev@gmail.com>
2014-01-16 16:34:38 -0800

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+ZurbStore::Application.config.secret_key_base = '408e2c11d19c294bc600ef1b28925a919aa105dc8b32ca0165b3a82724520bd6a1318c77d93fe5765361ba6d618ecd66bb6c89c01930b93694d3cc31ae3e6ac1'
---
+++ config/routes.rb
@@ -0,0 +1,4 @@
+ZurbStore::Application.routes.draw do
+  resources :products
+  root 'products#index'
+end
